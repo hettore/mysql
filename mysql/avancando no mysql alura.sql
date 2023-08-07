f773ceac1f728c1853997eda44367c11d79f6b69
Hettore Eduardo <duh.barretoo@gmail.com>
2023-08-07 17:22:14 -0300

    relembrando algumas consultas AND/ OR/ IN...

--- mysql/avancando no mysql alura.sql
+++ mysql/avancando no mysql alura.sql
@@ -24,3 +24,29 @@ SELECT * FROM tabela_de_produtos WHERE PRECO_DE_LISTA > 19.51;
 
 SELECT * FROM tabela_de_produtos WHERE PRECO_DE_LISTA between 19.49 AND  19.51;
 
+
+
+SELECT * FROM tabela_de_produtos WHERE SABOR = 'MANGA'
+OR TAMANHO = '470 ML';
+
+SELECT * FROM tabela_de_produtos WHERE SABOR = 'MANGA'
+AND TAMANHO = '470 ML';
+
+SELECT * FROM tabela_de_produtos WHERE NOT (SABOR = 'MANGA'
+AND TAMANHO = '470 ML');
+
+SELECT * FROM tabela_de_produtos WHERE NOT (SABOR = 'MANGA'
+OR TAMANHO = '470 ML');
+
+SELECT * FROM tabela_de_produtos WHERE SABOR = 'MANGA'
+AND NOT (TAMANHO = '470 ML');
+
+SELECT * FROM tabela_de_produtos WHERE SABOR IN ('LARANJA', 'MANGA');
+
+SELECT * FROM tabela_de_produtos WHERE SABOR = 'LARANJA' OR SABOR = 'MANGA';
+
+SELECT * FROM tabela_de_clientes WHERE CIDADE IN ('RIO DE JANEIRO', 'SAO PAULO')
+AND IDADE >= 20;
+
+SELECT * FROM tabela_de_clientes WHERE CIDADE IN ('RIO DE JANEIRO', 'SAO PAULO')
+AND (IDADE >= 19 AND IDADE <= 22);
