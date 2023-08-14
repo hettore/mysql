cc3227435175f2c5838cd901ef528cda24cbfe78
Hettore Eduardo <duh.barretoo@gmail.com>
2023-08-14 16:15:53 -0300

    Usando o UNION, subconsultas e view

--- mysql/avancando no mysql alura.sql
+++ mysql/avancando no mysql alura.sql
@@ -210,3 +210,57 @@ SELECT DISTINCT A.CPF, A.NOME, B.CPF FROM tabela_de_clientes A
 LEFT JOIN notas_fiscais B ON A.CPF = B.CPF
 WHERE B.CPF IS NULL AND YEAR(B.DATA_VENDA) = 2018;
 
+SELECT * FROM tabela_de_vendedores;
+
+SELECT count(*) FROM tabela_de_clientes;
+
+SELECT tabela_de_vendedores.BAIRRO,
+tabela_de_vendedores.NOME, DE_FERIAS,
+tabela_de_clientes.bairro,
+tabela_de_clientes.nome
+FROM tabela_de_vendedores INNER JOIN tabela_de_clientes
+ON tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO;
+
+SELECT DISTINCT BAIRRO FROM tabela_de_clientes;
+SELECT DISTINCT BAIRRO FROM tabela_de_vendedores;
+
+SELECT DISTINCT BAIRRO FROM tabela_de_clientes
+UNION ALL
+SELECT DISTINCT BAIRRO FROM tabela_de_vendedores;
+
+SELECT DISTINCT BAIRRO, NOME, 'CLIENTE' AS TIPO FROM tabela_de_clientes
+UNION 
+SELECT DISTINCT BAIRRO, NOME, 'VENDEDOR' AS TIPO FROM tabela_de_vendedores;
+
+SELECT DISTINCT BAIRRO, NOME, 'CLIENTE' AS TIPO_CLIENTE FROM tabela_de_clientes
+UNION 
+SELECT DISTINCT BAIRRO, NOME, 'VENDEDOR' AS TIPO_VENDEDOR FROM tabela_de_vendedores;
+
+SELECT DISTINCT BAIRRO FROM tabela_de_vendedores;
+
+SELECT * FROM tabela_de_clientes WHERE BAIRRO 
+IN ('Tijuca', 'Jardins', 'Copacabana', 'Santo amaro');
+
+SELECT * FROM tabela_de_clientes WHERE BAIRRO
+IN (SELECT DISTINCT BAIRRO FROM tabela_de_vendedores);
+
+SELECT X.EMBALAGEM, X.PRECO_MAXIMO FROM
+(SELECT EMBALAGEM, MAX(PRECO_DE_LISTA) AS PRECO_MAXIMO FROM tabela_de_produtos
+GROUP BY EMBALAGEM) X WHERE X.PRECO_MAXIMO >= 10;
+
+SELECT EMBALAGEM, MAX(PRECO_DE_LISTA) AS MAIOR_PRECO FROM tabela_de_produtos
+GROUP BY EMBALAGEM;
+
+SELECT X.EMBALAGEM, X.MAIOR_PRECO FROM
+vw_maiores_embalagens X WHERE X.MAIOR_PRECO >= 10;
+
+SELECT A.NOME_DO_PRODUTO, A.EMBALAGEM, A.PRECO_DE_LISTA, X.MAIOR_PRECO
+FROM tabela_de_produtos A
+INNER JOIN vw_maiores_embalagens X
+ON A.EMBALAGEM = X.EMBALAGEM;
+
+SELECT A.NOME_DO_PRODUTO, A.EMBALAGEM, A.PRECO_DE_LISTA, X.MAIOR_PRECO,
+((A.PRECO_DE_LISTA / X.MAIOR_PRECO)-1) * 100 AS PERCENTUAL
+FROM tabela_de_produtos A
+INNER JOIN vw_maiores_embalagens X
+ON A.EMBALAGEM = X.EMBALAGEM;
